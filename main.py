773d4cc80f4306872193a2f71d79d67151cca90d
Adrian030304 <adrianromanenco1@gmail.com>
2025-11-11 01:44:19 +0200

    implement tokenization

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import string
+import nltk
+from nltk.tokenize import TreebankWordTokenizer
+
+REMOVE_PUNCTUATION = str.maketrans({x: None for x in string.punctuation})
+TOKENIZER = TreebankWordTokenizer()
+
+example_doc = docs[1]
+
+example_doc_token = TOKENIZER.tokenize(
+    example_doc.translate(REMOVE_PUNCTUATION)
+)
+
+
+
